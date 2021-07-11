7d99cf66a8cda699ff74f4f8c30984dcb9c24661
Kali-Design <hm_leo@hotmail.com>
2021-07-12 00:23:47 +0200

    Working progress

--- migrations/2_deploy_depositnft.js
+++ migrations/2_deploy_depositnft.js
@@ -1,4 +1,4 @@
-const DepositNft = artifacts.require('DepositNft');
+const depositnft = artifacts.require('DepositNft');
 
 module.exports = async (deployer, accounts) => {
   await deployer.deploy(DepositNft, accounts[0], { from: accounts[0] });
